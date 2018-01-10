a344d3579c7fe89e80f8ba312d962dd096bb4acb
Deva <deva.palasingam@gmail.com>
2018-01-10 17:14:37 -0600

    added routing for landing page and search page

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import Landing from "./Landing";
+import Search from "./Search";
 
-const App = () => {
-  return (
+
+const App = () => (
+  <HashRouter>
     <div className="app">
-      <div className="landing">
-        <h1>Video App</h1>
-        <input type="text" placeholder="Search" />
-        <a>or Browse All</a>
-      </div>
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
     </div>
-  )
-};
+  </HashRouter>
+);
 
 export default class ClientApp extends React.Component {
   render () {
